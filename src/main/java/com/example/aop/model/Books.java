7972f182d356987042fc1c7d10663ca2878836ec
sivasubrahmanyam <sivasubrahmanyam.pagolu@techwave.net>
2022-06-11 13:49:38 +0530

    changes in Books model class

--- src/main/java/com/example/aop/model/Books.java
+++ src/main/java/com/example/aop/model/Books.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Table(name = "books")
 public class Books {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String author;
